a1362a1e9dee785b323cdaea6c5cb720b1ddb354
SahoriKikuchi <SahoriKikuchi@users.noreply.github.com>
2021-03-17 18:51:22 -0300

    Calculadora

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Calculator from './main/Calculator';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+     <Calculator/>
+  </div>
+
+ ,
   document.getElementById('root')
 );
 
